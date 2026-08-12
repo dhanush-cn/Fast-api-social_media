6855f8662533f31be17daadaa884687da4189baa
Dhanush <dhanushcn736@gmail.com>
2026-08-12 16:27:45 +0530

    Use CURRENT_TIMESTAMP in models.py for SQLite & PostgreSQL compatibility

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class Post(Base):
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default='TRUE', nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('CURRENT_TIMESTAMP'))
     owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
 
     owner = relationship("User")
@@ -23,7 +23,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('CURRENT_TIMESTAMP'))
 
 class Vote(Base):
     __tablename__ = "votes"
@@ -37,7 +37,7 @@ class Comment(Base):
     content = Column(String, nullable=False)
     post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), nullable=False)
     owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('CURRENT_TIMESTAMP'))
 
     owner = relationship("User")
     post = relationship("Post", back_populates="comments")
@@ -49,7 +49,7 @@ class DirectMessage(Base):
     sender_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     receiver_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     content = Column(String, nullable=False)
-    timestamp = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    timestamp = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('CURRENT_TIMESTAMP'))
     is_read = Column(Boolean, server_default='FALSE', nullable=False)
 
     sender = relationship("User", foreign_keys=[sender_id])
